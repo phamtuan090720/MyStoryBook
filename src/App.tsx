3ccc327aa51c16258138c793039097ee170b6a31
Phạm Tuân <52484769+phamtuan090720@users.noreply.github.com>
2022-07-26 23:58:07 +0700

    init button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Button from "components/Button/Button";
 function App() {
-  return <Button />;
+  return <Button>Button</Button>;
 }
 
 export default App;
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,50 @@
 import React from "react";
-
-export default function Button() {
-  return <div className="button">Button</div>;
+import styles from "./style.module.scss";
+interface ButtonProps {
+  className?: string;
+  onClick?: () => void;
+  variant?: "text" | "contained" | "outline";
+  theme?: "primary" | "secondary" | "danger";
+  size?: "small" | "medium" | "large";
+  disabled?: boolean;
+  children?: React.ReactNode;
+  startIcon?: React.ReactNode;
+  endIcon?: React.ReactNode;
 }
+let sizeClassEnum = {
+  small: `button__size--small`,
+  medium: `button__size--medium`,
+  large: `button__size--large`,
+};
+let themeEnum = {
+  primary: `button__theme--primary`,
+  secondary: `button__theme--secondary`,
+  danger: `button__theme--danger`,
+};
+const Button = ({
+  className = "",
+  children,
+  startIcon,
+  endIcon,
+  size = "small",
+  theme = "primary",
+}: ButtonProps) => {
+  return (
+    <button
+      className={[
+        className,
+        styles["button"],
+        styles[sizeClassEnum[size]],
+        styles[themeEnum[theme]],
+      ].join(" ")}
+    >
+      <div className={styles["content"]}>
+        <span className={styles["content__icon"]}>{startIcon}</span>
+        <span className={styles["content__text"]}>{children}</span>
+        <span className={styles["content__icon"]}> {endIcon}</span>
+      </div>
+    </button>
+  );
+};
+
+export default Button;
